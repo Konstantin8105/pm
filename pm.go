b1a3ea237bc7e5986143dd800d54e74ac512e541
Konstantin8105 <Konstantin8105@gmail.com>
2020-10-07 12:38:30 +0300

    init design

---
+++ pm.go
@@ -0,0 +1,430 @@
+package pm
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"runtime/debug"
+	"sort"
+	"time"
+
+	"github.com/Konstantin8105/errors"
+	"github.com/Konstantin8105/sparse"
+)
+
+// Config is default property of power method
+type Config struct {
+	// Maximal amount iteration
+	IterationMax uint64
+
+	// Iteration tolerance
+	// TODO (KI) : add comment about for each lambda precasion is lost
+	Tolerance float64
+}
+
+// Pm power method for approximating eigenvalues.
+type Pm struct {
+	a      *sparse.Matrix
+	ignore []int
+	config Config
+
+	// eigenvalue result of calculation
+	𝜦 float64
+
+	// eigenvector result of calculation
+	𝑿 []float64
+}
+
+// zeroize - set 0.0 in each element of slice
+func zeroize(x []float64) {
+	for i := range x {
+		x[i] = 0.0
+	}
+}
+
+// oneMax - modify slice with max value 1.0
+func oneMax(x []float64) {
+	// check input data
+	if len(x) == 0 {
+		return
+	}
+
+	// find max value
+	max := x[0]
+	min := x[0]
+	for i := range x {
+		if min < x[i] && x[i] < max {
+			continue
+		}
+		if x[i] < min {
+			min = x[i]
+		}
+		if x[i] > max {
+			max = x[i]
+		}
+	}
+	if math.Abs(min) > max {
+		max = min
+	}
+	// if max == 1.0 {
+	// 	return
+	// }
+	//
+	// if max == 66.66 , then max = 100
+	// if max == 0.006 , then max = 0.01
+	// if max > 0 {
+	// 	max = math.Pow(10.0, float64(int(math.Log10(max))))
+	// } else {
+	// 	max = -math.Pow(10.0, float64(int(math.Log10(-max))))
+	// }
+
+	// modification
+	for i := range x {
+		// TODO (KI) : need parallel
+		x[i] /= max
+	}
+}
+
+// PM is power method for approximating eigenvalues.
+// Find `dominant eigenvalue` of matrix A.
+// List `ignore` is list of ignore row and column in calculation.
+//
+//	Algorith : Power Method
+//	x(0) // initial vector
+//	k = 1
+//	for δ < ɛ {
+//		x(k) = A · x(k-1)
+//		δ = || x(k) - x(k-1) ||1
+//		k = k + 1
+//	}
+//
+// See next articles:
+//
+//	1. Sepandar D. Kamvar, Taher H. Haveliwala, Christopher D. Manning, Gene H. Golub
+//	"Extrapolation Methods for Accelerating PageRank Computations"
+//
+func (pm *Pm) Factorize(A *sparse.Matrix, config *Config, ignore ...int) (err error) {
+	// check input data
+	et := errors.New("Function LU.Factorize: check input data")
+	if A == nil {
+		_ = et.Add(fmt.Errorf("matrix A is nil"))
+	}
+	if A != nil {
+		rows, columns := A.Dims()
+		if rows <= 0 {
+			_ = et.Add(fmt.Errorf("matrix row A is valid: %v", rows))
+		}
+		if columns <= 0 {
+			_ = et.Add(fmt.Errorf("matrix columns A is valid: %v", columns))
+		}
+		if A.IsTriplet() {
+	 	_ = et.Add(fmt.Errorf("matrix A is not CSC(Compressed Sparse Column) format"))
+		}
+	}
+
+	// sort ignore list
+	(sort.IntSlice(ignore)).Sort()
+	if len(ignore) > 0 && A != nil {
+		rows, columns := A.Dims()
+		if ignore[0] < 0 {
+			_ = et.Add(fmt.Errorf("ignore list have index less zero: %d", ignore[0]))
+		}
+		if ignore[len(ignore)-1] >= rows || ignore[len(ignore)-1] >= columns {
+			_ = et.Add(fmt.Errorf("ignore list have index outside matrix"))
+		}
+	}
+
+	if et.IsError() {
+		err = et
+		return
+	}
+
+	// panic free. replace to stacktrace
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("stacktrace from panic: %s\n", debug.Stack())
+		}
+	}()
+
+	// remove duplicate from `ignore` list
+	if len(ignore) > 0 {
+		list := append([]int{}, ignore[0])
+		for i := 1; i < len(ignore); i++ {
+			if ignore[i-1] == ignore[i] {
+				continue
+			}
+			list = append(list, ignore[i])
+		}
+		list, ignore = ignore, list
+		//	cs_free(list)
+	}
+
+	// coping matrix A without ignored rows and columns
+	C, _ := A.Copy()
+// if len(ignore) > 0 {
+// 	_, err := sparse.Fkeep(C, func(i, j int, x float64) bool {
+// 		var found bool
+// 		for k := range ignore {
+// 			if i == ignore[k] || j == ignore[k] {
+// 				found = true
+// 				break
+// 			}
+// 		}
+// 		return !found // if not found , then keep value
+// 	})
+// 	if err != nil {
+// 		return err
+// 	}
+// 
+// 	// remove empty column
+// 	// var pnew []int
+// 	// for j := 0; j < C.n; j++ {
+// 	// 	var found bool
+// 	// 	for k := range ignore {
+// 	// 		if j == ignore[k] {
+// 	// 			found = true
+// 	// 			break
+// 	// 		}
+// 	// 	}
+// 	// 	if found {
+// 	// 		continue
+// 	// 	}
+// 	// 	pnew = append(pnew, C.p[j])
+// 	// }
+// 	// C.p = append(pnew, C.p[C.n])
+// 
+// 	// recalculate amount rows and columns
+// 	// C.n -= len(ignore)
+// 	// C.m -= len(ignore)
+// 
+// 	// // recalculate vector i
+// 	// for j := 0; j < C.n; j++ {
+// 	// 	for p := C.p[j]; p < C.p[j+1]; p++ {
+// 	// 		incr := 0
+// 	// 		for k := range ignore {
+// 	// 			if C.i[p] > ignore[k] {
+// 	// 				incr++
+// 	// 			}
+// 	// 		}
+// 	// 		C.i[p] -= incr
+// 	// 	}
+// 	// }
+// }
+
+	// minimal configuration
+	if config == nil {
+		config = &Config{
+			IterationMax: 500,
+			Tolerance:    1e-5,
+		}
+	}
+
+	// store
+	pm.ignore = ignore
+	pm.a = C
+	pm.config = *config
+
+	// matrix A must have diagonal element
+	// for j := 0; j < pm.a.n; j++ {
+	// 	found := false
+	// 	for p := pm.a.p[j]; p < pm.a.p[j+1]; p++ {
+	// 		if j != pm.a.i[p] {
+	// 			continue
+	// 		}
+	// 		// only diagonal element
+	// 		found = true
+	// 	}
+	// 	if !found {
+	// 		// diagonal element is not found in that column
+	// 		pm.a.inject(j, j, 0.0)
+	// 	}
+	// }
+
+	return
+}
+
+// TODO (KI) : add research PM from input values
+// TODO (KI) : [7 4 1; 4 4 4; 1 4 7]
+// TODO (KI) : xo = [1 2 3]T
+// TODO (KI) : xo = [0 1 -1]T
+
+// Eigen calculate eigenvalue
+func (pm *Pm) Eigen() (err error) {
+	// panic free. replace to stacktrace
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("stacktrace from panic: %s\n", debug.Stack())
+		}
+	}()
+
+	// workspace
+	var (
+		rows, cols = pm.a.Dims()
+		x          = make([]float64, rows) // len(pm.ignore))
+		xNext      = make([]float64, rows) // len(pm.ignore))
+	)
+	_ = cols
+	x = x[:rows]
+	xNext = xNext[:rows]
+
+	rand.Seed(time.Now().UnixNano())
+
+	for i := range x {
+		x[i] = rand.Float64() - 0.5
+	}
+
+	dlast := 1.0
+	oneMax(x)
+
+	// iteration value
+	var iter uint64
+
+	// main iteration function
+	iteration := func() {
+		// x(k) = A*x(k-1) - (∑(𝛌(j)*[x(j)]*[x(j)Transpose])) * x(k-1),
+		//        |      |   |                                       |
+		//        +------+   +---------------------------------------+
+		//          part1               part 2
+		// where j = 0 ... k-1
+		oneMax(x)
+		for _, i := range pm.ignore{
+			x[i] = 0.0
+		}
+		zeroize(xNext)
+		_, _ = sparse.Fkeep(pm.a, func(i, j int, val float64) bool {
+			xNext[i] += val * x[j]
+			return true
+		})
+
+		// value pm.E.X without ignore elements
+		EX := make([]float64, rows)
+		if len(pm.ignore) == 0 { // ignore list is empty
+			copy(EX, pm.𝑿)
+		} else {
+			// TODO (KI)
+			// panic("TODO")
+		}
+		for row := 0; row < rows; row++ {
+			for col := 0; col < rows; col++ {
+				xNext[row] -= pm.𝜦 * EX[row] * EX[col] * x[col]
+			}
+		}
+
+		// post work
+		x, xNext = xNext, x
+	}
+
+	// calculation
+	for {
+		// main iteration function
+		oneMax(x)
+		iteration()
+
+		// first norm of vector
+		// link: https://en.wikipedia.org/wiki/Norm_(mathematics)#Taxicab_norm_or_Manhattan_norm
+		oneMax(x)
+		oneMax(xNext)
+		var d float64
+		for i := range x {
+			d += math.Abs(x[i] - xNext[i])
+		}
+
+		if math.Abs(dlast-d) < pm.config.Tolerance {
+			// tolerance breaking
+			break
+		}
+		if iter >= pm.config.IterationMax {
+			err = ErrorPm{
+				Iteration:    iter,
+				IterationMax: pm.config.IterationMax,
+				Delta:        dlast - d,
+				Tolerance:    pm.config.Tolerance,
+				err:          fmt.Errorf("iteration limit"),
+			}
+			return
+		}
+
+		dlast = d
+		iter++
+	}
+
+	// compute the Rayleigh quotient
+	// 𝛌 = (Ax · x) / (x · x)
+
+	// calculation of Ax is ignore and takes value x(k-1)
+
+	// up : Ax · x
+	iteration()
+
+	var up float64
+	for i := range x {
+		up += x[i] * xNext[i]
+	}
+
+	// down : x · x
+	oneMax(x)
+	var down float64
+	for i := range x {
+		down += x[i] * x[i]
+	}
+
+	if math.Abs(down) == 0.0 || math.IsNaN(up) {
+		return fmt.Errorf("Not acceptable value")
+	}
+
+	// calculation eigenvalue
+	𝛌 := up / down
+
+	// change workspace with length of ignore list
+	//  x = append(x, make([]float64, len(pm.ignore))...)
+
+	// ignore list
+// if len(pm.ignore) > 0 {
+// 	// decomperess vector `x`
+// 	// short x vector: x = [1 2]
+// 	// ignore list   :     [0 2 4]
+// 	// result        : x = [0 1 0 2 0]
+// 	counter := 0
+// 	for i := len(x) - 1; i >= 0; i-- {
+// 		var found bool
+// 		for k := range pm.ignore {
+// 			if i == pm.ignore[k] {
+// 				found = true
+// 				break
+// 			}
+// 		}
+// 		if found {
+// 			x[i] = 0
+// 			continue
+// 		}
+// 		counter++
+// 		x[i] = x[len(x)-len(pm.ignore)-counter]
+// 	}
+// }
+
+	oneMax(x)
+
+	pm.𝜦 = 𝛌 // eigenvalue
+	pm.𝑿 = x // eigenvector
+
+	return
+}
+
+// ErrorPm error of power method
+type ErrorPm struct {
+	Iteration    uint64
+	IterationMax uint64
+	Delta        float64
+	Tolerance    float64
+	err          error
+}
+
+func (e ErrorPm) Error() string {
+	et := errors.New("Power method error")
+	_ = et.Add(fmt.Errorf("Iteration: %d", e.Iteration))
+	_ = et.Add(fmt.Errorf("Max.Iteration: %d", e.IterationMax))
+	_ = et.Add(fmt.Errorf("Delta tolerance: %5e", e.Tolerance))
+	_ = et.Add(fmt.Errorf("Tolerance: %5e", e.Tolerance))
+	_ = et.Add(e.err)
+	return et.Error()
+}
---
+++ pm_test.go
@@ -0,0 +1,410 @@
+package pm
+
+import (
+	"bytes"
+	"math"
+	"testing"
+
+	"github.com/Konstantin8105/sparse"
+)
+
+// func ExamplePm() {
+// 	T, err := NewTriplet()
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	// storage
+// 	errs := []error{
+// 		Entry(T, 0, 0, 1.0000),
+// 		Entry(T, 0, 1, 2.0000),
+//
+// 		Entry(T, 1, 0, -2.000),
+// 		Entry(T, 1, 1, 1.0000),
+// 		Entry(T, 1, 2, 2.0000),
+//
+// 		Entry(T, 2, 0, 1.0000),
+// 		Entry(T, 2, 1, 3.0000),
+// 		Entry(T, 2, 2, 1.0000),
+// 	}
+// 	for i := range errs {
+// 		if errs[i] != nil {
+// 			panic(errs[i])
+// 		}
+// 	}
+//
+// 	// compress
+// 	A, err := Compress(T)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+//
+// 	var pm Pm
+// 	err = pm.Factorize(A, &Config{
+// 		IterationMax: 1000000000,
+// 		Tolerance:    1e-6,
+// 	})
+// 	if err != nil {
+// 		panic(err)
+// 	}
+//
+// 	err = pm.Eigen()
+// 	if err != nil {
+// 		panic(err)
+// 	}
+//
+// 	// Output:
+// }
+
+// func BenchmarkPM(b *testing.B) {
+// 	T, err := NewTriplet()
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	// storage
+// 	errs := []error{
+// 		Entry(T, 0, 3, 0.9090),
+// 		Entry(T, 0, 4, 0.0910),
+//
+// 		Entry(T, 1, 0, 0.0002),
+// 		Entry(T, 1, 2, 0.9998),
+//
+// 		Entry(T, 2, 1, 0.9998),
+// 		Entry(T, 2, 3, 0.0002),
+//
+// 		Entry(T, 3, 0, 0.6690),
+// 		Entry(T, 3, 4, 0.3310),
+//
+// 		Entry(T, 4, 0, 0.9989),
+// 		Entry(T, 4, 1, 0.0011),
+// 	}
+// 	for i := range errs {
+// 		if errs[i] != nil {
+// 			panic(errs[i])
+// 		}
+// 	}
+//
+// 	// compress
+// 	A, err := Compress(T)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+//
+// 	for i := 0; i < b.N; i++ {
+// 		var pm Pm
+// 		err = pm.Factorize(A, &Config{
+// 			IterationMax: 1000000000,
+// 			Tolerance:    1e-6,
+// 		})
+// 		if err != nil {
+// 			panic(err)
+// 		}
+//
+// 		err = pm.Eigen()
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 	}
+// }
+
+func TestPm(t *testing.T) {
+	// tolerance
+	eps := 1e-7
+
+	compare := func(t *testing.T, v1, v2 float64, name string) {
+		v1 = math.Abs(v1)
+		v2 = math.Abs(v2)
+		var e float64
+		if v1 != 0.0 {
+			e = math.Abs(v1-v2) / v1
+			if e > eps {
+				t.Errorf("Not correct `%s`: %e != %e. Diff = %e", name, v1, v2, e)
+			}
+		} else if v2 != 0.0 {
+			e = math.Abs(v1-v2) / v2
+			if e > eps {
+				t.Errorf("Not correct `%s`: %e != %e. Diff = %e", name, v1, v2, e)
+			}
+		} else {
+			e = math.Abs(v1 - v2)
+			if e > eps {
+				t.Errorf("Not correct `%s`: %e != %e. Diff = %e", name, v1, v2, e)
+			}
+		}
+		t.Logf("Precition `%s` = %.3e%%", name, e*100.0)
+	}
+	compareSlice := func(t *testing.T, v1, v2 []float64, name string) {
+		if len(v1) != len(v2) {
+			t.Fatalf("Not same len of slices: %d != %d\n%v\n%v",
+				len(v1), len(v2), v1, v2)
+		}
+		for i := range v1 {
+			compare(t, v1[i], v2[i], name)
+		}
+	}
+
+	t.Run("specific2x2", func(t *testing.T) {
+		T, err := sparse.NewTriplet()
+		if err != nil {
+			panic(err)
+		}
+		// storage
+		errs := []error{
+			sparse.Entry(T, 0, 0, 13),
+			sparse.Entry(T, 0, 1, 5),
+
+			sparse.Entry(T, 1, 0, 2),
+			sparse.Entry(T, 1, 1, 4),
+		}
+		for i := range errs {
+			if errs[i] != nil {
+				panic(errs[i])
+			}
+		}
+
+		// compress
+		A, err := sparse.Compress(T)
+		if err != nil {
+			panic(err)
+		}
+
+		var pm Pm
+		err = pm.Factorize(A, &Config{
+			IterationMax: 1000,
+			Tolerance:    1e-8,
+		})
+		if err != nil {
+			panic(err)
+		}
+
+		err = pm.Eigen()
+		if err != nil {
+			panic(err)
+		}
+
+		t.Logf("𝑿 = %v", pm.𝑿)
+		t.Logf("𝜦 = %v", pm.𝜦)
+
+		// result checking
+		𝛌Expect := 14.0
+		xExpect := []float64{1.0, 0.2}
+		compare(t, pm.𝜦, 𝛌Expect, "𝛌")
+		compareSlice(t, pm.𝑿, xExpect, "𝑿")
+	})
+
+	t.Run("2x2", func(t *testing.T) {
+		T, err := sparse.NewTriplet()
+		if err != nil {
+			panic(err)
+		}
+		// storage
+		errs := []error{
+			sparse.Entry(T, 0, 0, 2000),
+
+			sparse.Entry(T, 1, 1, 2),
+			sparse.Entry(T, 1, 3, -12),
+
+			sparse.Entry(T, 2, 2, 2000),
+
+			sparse.Entry(T, 3, 1, 1),
+			sparse.Entry(T, 3, 3, -5),
+
+			sparse.Entry(T, 4, 4, 2000),
+		}
+		for i := range errs {
+			if errs[i] != nil {
+				panic(errs[i])
+			}
+		}
+
+		// compress
+		A, err := sparse.Compress(T)
+		if err != nil {
+			panic(err)
+		}
+
+		var pm Pm
+		err = pm.Factorize(A, &Config{
+			IterationMax: 1000,
+			Tolerance:    1e-8,
+		}, 0, 4, 2, 0, 2, 4)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		err = pm.Eigen()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		t.Logf("𝑿 = %v", pm.𝑿)
+		t.Logf("𝜦 = %v", pm.𝜦)
+
+		// result checking
+		𝛌Expect := -2.0
+		xExpect := []float64{0.0, 1.0, 0.0, 1.0 / 3.0, 0.0}
+		compare(t, pm.𝜦, 𝛌Expect, "𝛌")
+		compareSlice(t, pm.𝑿, xExpect, "𝑿")
+	})
+	t.Run("3x3", func(t *testing.T) {
+		T, err := sparse.NewTriplet()
+		if err != nil {
+			panic(err)
+		}
+		// storage
+		errs := []error{
+			sparse.Entry(T, 0, 0, 2),
+			sparse.Entry(T, 0, 1, 3),
+
+			sparse.Entry(T, 1, 0, 3),
+			sparse.Entry(T, 1, 2, 5),
+
+			sparse.Entry(T, 2, 1, 5),
+			sparse.Entry(T, 2, 2, 2),
+		}
+		for i := range errs {
+			if errs[i] != nil {
+				panic(errs[i])
+			}
+		}
+
+		// compress
+		A, err := sparse.Compress(T)
+		if err != nil {
+			panic(err)
+		}
+
+		var pm Pm
+		err = pm.Factorize(A, &Config{
+			IterationMax: 10000000,
+			Tolerance:    1e-8,
+		})
+
+		if err != nil {
+			panic(err)
+		}
+
+		err = pm.Eigen()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		t.Logf("𝑿 = %v", pm.𝑿)
+		t.Logf("𝜦 = %v", pm.𝜦)
+
+		// result checking
+		𝛌Expect := 6.916079682949667
+		compare(t, pm.𝜦, 𝛌Expect, "𝛌")
+	})
+
+	t.Run("2x2: IterationMax error", func(t *testing.T) {
+		// too small amount iterations
+
+		T, err := sparse.NewTriplet()
+		if err != nil {
+			panic(err)
+		}
+		// storage
+		errs := []error{
+			sparse.Entry(T, 0, 0, 2),
+			sparse.Entry(T, 0, 1, -12),
+			sparse.Entry(T, 1, 0, 1),
+			sparse.Entry(T, 1, 1, -5),
+		}
+		for i := range errs {
+			if errs[i] != nil {
+				panic(errs[i])
+			}
+		}
+
+		// compress
+		A, err := sparse.Compress(T)
+		if err != nil {
+			panic(err)
+		}
+
+		var pm Pm
+		err = pm.Factorize(A, &Config{IterationMax: 1})
+		if err != nil {
+			panic(err)
+		}
+
+		err = pm.Eigen()
+		if err == nil {
+			t.Errorf("cannot check max iter")
+		}
+		t.Log(err)
+		t.Log(err.Error())
+	})
+	t.Run("2x2: Iteration error", func(t *testing.T) {
+		T, err := sparse.NewTriplet()
+		if err != nil {
+			panic(err)
+		}
+		// storage
+		errs := []error{
+			sparse.Entry(T, 0, 0, 2),
+			sparse.Entry(T, 0, 1, -12),
+			sparse.Entry(T, 1, 0, 1),
+			sparse.Entry(T, 1, 1, -5),
+		}
+		for i := range errs {
+			if errs[i] != nil {
+				panic(errs[i])
+			}
+		}
+		// compress
+		A, err := sparse.Compress(T)
+		if err != nil {
+			panic(err)
+		}
+		var pm Pm
+		_ = pm.Factorize(A, nil)
+	})
+	t.Run("oneMax: error", func(t *testing.T) {
+		defer func() {
+			if r := recover(); r != nil {
+				t.Fatalf("Not valid")
+			}
+		}()
+		oneMax(nil)
+	})
+	t.Run("A is nil", func(t *testing.T) {
+		var pm Pm
+		err := pm.Factorize(nil, nil)
+		if err == nil {
+			t.Fatalf("not check")
+		}
+		t.Log(err)
+	})
+	t.Run("Triplet", func(t *testing.T) {
+		var stdin bytes.Buffer
+		stdin.WriteString("0 0 1\n 0 1 2\n 1 0 3\n 1 1 4")
+		T, err := sparse.Load(&stdin)
+		if err != nil {
+			panic(err)
+		}
+		var pm Pm
+		err = pm.Factorize((*sparse.Matrix)(T), nil)
+		if err == nil {
+			t.Fatalf("not check")
+		}
+		t.Log(err)
+	})
+	t.Run("Small", func(t *testing.T) {
+		var stdin bytes.Buffer
+		stdin.WriteString("")
+		T, err := sparse.Load(&stdin)
+		if err != nil {
+			panic(err)
+		}
+		A, err := sparse.Compress(T)
+		if err != nil {
+			panic(err)
+		}
+		var pm Pm
+		if err = pm.Factorize(A, nil, -1, 2, 100, 9); err == nil {
+			t.Fatalf("not check")
+		}
+		t.Log(err)
+	})
+}
